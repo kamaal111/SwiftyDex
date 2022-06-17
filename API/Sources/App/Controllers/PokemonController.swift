a685afe3b8318e410dbf9176e54af57e307c7376
Kamaal Farah <kfarah@box.com>
2022-06-17 21:41:03 +0200

    Better error handling in pokemon controller

--- API/Sources/App/Controllers/PokemonController.swift
+++ API/Sources/App/Controllers/PokemonController.swift
@@ -9,6 +9,7 @@ import Vapor
 import PokeAPI
 import APIModels
 import PokeAPIModels
+import ClientKit
 
 struct PokemonController: Controller {
     private let pokeAPI: PokeAPI
@@ -25,20 +26,42 @@ struct PokemonController: Controller {
     }
 
     func getPokedex(request: Request) async throws -> [Pokemon] {
-        guard let id = request.parameters.get("id"), let id = Int(id) else { throw Abort(.badRequest) }
+        guard let id = request.parameters.get("id"), let id = Int(id)
+        else { throw Abort(.badRequest, reason: "Invalid ID provided") }
 
         let result = await pokeAPI.pokedex.getPokedex(by: id)
         let pokedexResponse: PokedexResponse
         switch result {
         case .failure(let failure):
-            logger.error("failed with \(failure)")
-            throw Abort(.failedDependency, reason: "Poke API failed")
+            throw returnClientErrorFromPokeAPI(failure)
         case .success(let success):
             pokedexResponse = success
         }
 
         return pokedexResponse.pokemonEntries
             .compactMap({ Pokemon(fromEntry: $0) })
+    }
+
+    private func returnClientErrorFromPokeAPI(_ error: ClientKitErrors) -> AbortError {
+        switch error {
+        case .parsingError(error: let error):
+            logger.error("failed parsing error in PokeAPI; \(error)")
+            return Abort(.internalServerError, reason: "Something went wrong")
+        case .invalidURL(url: let url):
+            logger.error("unknown url in PokeAPI \(url)")
+            return Abort(.internalServerError, reason: "Something went wrong")
+        case .notAValidJSON:
+            return Abort(.internalServerError, reason: "Something went wrong")
+        case .generalError(error: let error):
+            logger.error("failed error in PokeAPI; \(error)")
+            return Abort(.internalServerError, reason: "Something went wrong")
+        case .responseError(message: let message, code: let code):
+            logger.error("response failure in PokeAPI; \(message)")
+            if code >= 500 {
+                return Abort(.failedDependency, reason: "Poke API failed")
+            }
+            return Abort(HTTPStatus(statusCode: code), reason: "Something went wrong")
+        }
     }
 }
 
